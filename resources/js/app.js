1ff47db8908c4a65f28b63f9a0e763f95348a9f9
albanonacho <ignacio@nasoft.com.ar>
2023-02-08 09:43:27 -0300

    Clase 109

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,45 @@
-import './bootstrap';
+import Dropzone from "dropzone";
+
+Dropzone.autoDiscover = false;
+
+const dropzone = new Dropzone('#dropzone', {
+    dictDefaultMessage: 'Sube aquí tu imagen',
+    acceptedFiles: ".png, .jpg, .jpeg, .gif",
+    addRemoveLinks: true,
+    dictRemoveFile: 'Borrar Archivo',
+    maxFiles: 1,
+    uploadMultiple: false,
+    init: function() {
+        console.log('init');
+        if (document.querySelector('[name="imagen"]').value.trim()) {
+
+            console.log('entrooo');
+
+            const imagenPublicada = {}
+            imagenPublicada.size = 1234;
+            imagenPublicada.name = document.querySelector('[name="imagen"]').value;
+
+            console.log(imagenPublicada)
+
+            this.options.addedfile.call(this, imagenPublicada);
+            this.options.thumbnail.call(
+                this, 
+                imagenPublicada, 
+                `/uploads/${imagenPublicada.name}`
+            );
+
+            imagenPublicada.previewElement.classList.add(
+                'dz-success',
+                'dz-complete'
+            )
+        }
+    }
+});
+
+dropzone.on("success", function(file, response){
+    document.querySelector('[name="imagen"]').value = response.imagen;
+});
+
+dropzone.on("removedfile", function(){
+    document.querySelector('[name="imagen"]').value = '';
+});
